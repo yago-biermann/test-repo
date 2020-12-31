68fd943a515b5c64a4121bade50f1bf7de635763
yago-biermann <ckyagock@gmail.com>
2020-12-31 17:03:58 -0300

    commit number: 20

--- main.ps1
+++ main.ps1
@@ -26,8 +26,9 @@ function MakeCommit {
     $CommitNumber = GetLastCommit
     for ($i = 1; $i -le $TimesOfCommit; $i++) {
         git add .
-        git commit -m "commit number: $($CommitNumber)" | Clear-Host;
-        git push origin main | Clear-Host;
+        Clear-Host
+        git commit -m "commit number: $($CommitNumber)"
+        git push origin main
         Write-Host "Pushing the commits to remote repository."
         Write-Host "Commit Number: $($CommitNumber)" 
         Add-Content -Path "./files/commits.txt" -Value "Commit Number: $($CommitNumber +1)"
